7ca98808e2eefdb1e414b6ac34522413114fb847
Karlos Santos <6723574+louisgv@users.noreply.github.com>
2021-09-04 17:43:18 -0400

    Ready to publish

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-var FindStale = require('./lib/find-stale.js');
-var utils = require('./lib/utils.js');
+var FindStale = require("./lib/find-stale.js");
+var utils = require("./lib/utils.js");
 
-var argv = require('minimist')(process.argv, {
-  string: 'remote',
-  boolean: ['prune', 'force'],
-  alias: {p: "prune", f: "force", r: "remote"},
-  'default': {
-    'remote': 'origin',
-    'force': false
-  }
+var argv = require("minimist")(process.argv, {
+  string: "remote",
+  alias: { r: "remote" },
+  default: {
+    remote: "origin",
+  },
 });
 
-var options = ['prune', 'p', 'force', 'f', 'remote', 'r', '_'];
+var options = ["remote", "r", "_"];
 var hasInvalidParams = Object.keys(argv).some(function (name) {
-  return (options.indexOf(name) == -1);
+  return options.indexOf(name) == -1;
 });
 
 if (hasInvalidParams) {
-  console.info('Usage: git removed-branches [-p|--prune] [-f|--force] [-r|--remote <remote>]');
+  console.info("Usage: grbpf [-r|--remote <remote>]");
 } else {
   // check for git repository
-  var exec = utils.asyncExec(['git', 'rev-parse', '--show-toplevel']);
+  var exec = utils.asyncExec(["git", "rev-parse", "--show-toplevel"]);
   var obj = new FindStale({
-    remove: argv.prune,
-    force: argv.force,
-    remote: argv.remote
+    remove: true,
+    force: true,
+    remote: argv.remote,
   });
-  
+
   exec(function (err, stdout, stderr) {
     if (err) {
       console.error(err.message);
